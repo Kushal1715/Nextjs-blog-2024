1e5286dd1c99cf8ee5f4a049f1cd224e0095965d
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-22 21:56:40 +0545

    connecting to mongodb

---
+++ nextjs-blog-2024/src/database/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+
+const connectToDB = async () => {
+  const connectionUrl = "mongodb+srv://kushal:<db_password>@nextjs-blog-2024.ety0c.mongodb.net/"
+
+  mongoose
+    .connect(connectionUrl)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error))
+
+}
+
+export default connectToDB;
+
