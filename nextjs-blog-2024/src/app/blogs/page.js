68ac5571ab80dbcfd69c724750404d1867f2cc1a
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-23 23:14:21 +0545

    fetching blogs from db

--- nextjs-blog-2024/src/app/blogs/page.js
+++ nextjs-blog-2024/src/app/blogs/page.js
@@ -2,13 +2,20 @@ import BlogOverview from "@/components/blog-overview";
 
 async function getBlogs() {
   try {
-
+    const response = await fetch('http://localhost:3000/api/get-blogs');
+    const result = await response.json();
+    if (result?.data) {
+      return result.data;
+    }
   } catch (e) {
-    throw new Errow(e)
+    throw new Error(e)
   }
 }
 
-export default function Blogs() {
+export default async function Blogs() {
+
+  const blogs = await getBlogs();
+  console.log(blogs)
   return (
     <BlogOverview />
   )
