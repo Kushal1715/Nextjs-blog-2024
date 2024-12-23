06f2d85c9017b02bf2afe72bcbe79599c6eebcb7
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-23 16:06:00 +0545

    making api route to add blogs

---
+++ nextjs-blog-2024/src/app/api/add-blog/route.js
@@ -0,0 +1,49 @@
+import connectToDB from "@/database"
+import Blog from "@/models/blog";
+import Joi from "joi";
+import { NextResponse } from "next/server"
+
+const AddNewBlog = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required();
+})
+
+
+
+export async function POST(req) {
+  try {
+    await connectToDB();
+
+    const extractBlogData = await req.json();
+    const { title, description } = extractBlogData;
+    const { error } = AddNewBlog.validate({ title, description });
+
+    if (error) {
+      return NextResponse.json({
+        success: false,
+        message: error.details[0].message
+      })
+    }
+
+    const newlyCreatedBlogData = await Blog.create(extractBlogData)
+
+    if (newlyCreatedBlogData) {
+      return NextResponse.json({
+        success: true,
+        message: 'new blog added successfully'
+      })
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: 'something went wrong please try again'
+      })
+    }
+
+  } catch (e) {
+    console.log(e)
+    return NextResponse.json({
+      success: false,
+      message: 'something went wrong please try again'
+    })
+  }
+}
